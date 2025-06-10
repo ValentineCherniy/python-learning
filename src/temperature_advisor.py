816930165dc5e08cc76683470d96e5c7ffdf3340
Valentine Cherniy <cherniy.valentin@gmail.com>
2025-06-10 14:57:17 +0300

    temperature_advisor file

---
+++ src/temperature_advisor.py
@@ -0,0 +1,19 @@
+def main ():
+    temperature = int(input("Enter temperature in Celsius: "))
+
+    try:
+        if temperature < 0:
+            print("Wear a heavy coat.")
+        elif 0 <= temperature <= 10:
+            print("Wear a coat.")
+        elif 11 <= temperature <= 20:
+            print("Wear a jacket.")
+        elif 21 <= temperature <= 30:
+            print("Wear a t-shirt.")
+        else:
+            print("Stay hydrated!")
+    except ValueError:
+        print("Invalid input. Please enter a valid integer for temperature.")
+        return
+if __name__ == "__main__":
+    main()
