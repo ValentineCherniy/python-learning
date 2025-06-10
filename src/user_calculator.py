aa167de19f44451e172f31ab8d7206b7aa6d9ab7
Valentine Cherniy <cherniy.valentin@gmail.com>
2025-06-10 14:36:56 +0300

    move calculations to try block

--- src/user_calculator.py
+++ src/user_calculator.py
@@ -1,28 +1,25 @@
 def main (): 
-    n1 = input("Enter first number: ")
-    n2 = input("Enter second number: ")
+    n1 = float(input("Enter first number: "))
+    n2 = float(input("Enter second number: "))
     op = input("Enter operation (+, -, /, *): ")
-    res = 0
     result = 0
 
     try:
-        res = float(result)
+        if op == "+":
+            result = n1 + n2
+        elif op == "-":
+            result = n1 - n2
+        elif op == "/":
+            result = n1 / n2
+        elif op == "*":
+            result = n1 * n2
+        print(f"result = {result}")
     except ValueError:
         print ("Invalid input")
         return
     except ZeroDivisionError:
         print ("Division by zero is not allowed")
         return
-    
-    if op == "+":
-        result = n1 + n2
-    elif op == "-":
-        result = n1 - n2
-    elif op == "/":
-        result = n1 / n2
-    elif op == "*":
-        result = n1 * n2
-    print(f"result = {res}")
 
 
 if __name__ == "__main__":
